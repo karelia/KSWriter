fb7ad34e3e2183cf38c37ec8bdcc6d80c9f8c74f
Dan Wood <dwood@karelia.com>
2012-01-02 17:03:08 -0800

    2012 copyright update

--- KSOutputStreamWriter.h
+++ KSOutputStreamWriter.h
@@ -3,7 +3,7 @@
 //  Sandvox
 //
 //  Created by Mike on 10/03/2011.
-//  Copyright 2011 Karelia Software. All rights reserved.
+//  Copyright 2011-2012 Karelia Software. All rights reserved.
 //
 //  Converts incoming strings to data and writes them to an NSOutputStream
 
