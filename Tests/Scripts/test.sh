e47360bf7ef7a0783c1de87fa1a7066bedc3818b
Sam Deane <sam@elegantchaos.com>
2012-02-20 21:06:19 +0000

    more script tweaks

--- Tests/Scripts/test.sh
+++ Tests/Scripts/test.sh
@@ -5,7 +5,7 @@ config="-configuration Debug"
 sdkMac="macosx"
 options="clean build TEST_AFTER_BUILD=YES"
 
-CONVERT_OUTPUT="${base}/ocunit2junit.rb"
+CONVERT_OUTPUT="$base/ocunit2junit.rb"
 
 # build & run the tests
-xcodebuild -target "KSWriterTests" $config -sdk "$sdkMac" ${options} | "${CONVERT_OUTPUT}"
+xcodebuild -target "KSWriterTests" $config -sdk "$sdkMac" $options | "$CONVERT_OUTPUT"
