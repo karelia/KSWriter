92836c90cd18508a4cf9db3db4ba8cb872e1f9a1
Sam Deane <sam@elegantchaos.com>
2012-02-20 21:03:10 +0000

    updated test script to work ok with Xcode 4.4's xcodebuild (which seems not to run tests by default)

--- Tests/Scripts/test.sh
+++ Tests/Scripts/test.sh
@@ -3,6 +3,9 @@ echo Testing Mac
 base=`dirname $0`
 config="-configuration Debug"
 sdkMac="macosx"
+options="clean build TEST_AFTER_BUILD=YES"
+
+CONVERT_OUTPUT="${base}/ocunit2junit.rb"
 
 # build & run the tests
-xcodebuild -target "KSWriterTests" $config -sdk "$sdkMac" clean build | "${base}/ocunit2junit.rb"
+xcodebuild -target "KSWriterTests" $config -sdk "$sdkMac" ${options} | "${CONVERT_OUTPUT}"
