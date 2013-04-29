dc82a5202259575a496c41de5eb7c2d31215860d
Mike Abdullah <me@mikeabdullah.net>
2013-04-29 14:53:38 +0100

    Test harness that creates writer and output for us

--- Tests/KSWriterTests/KSBufferingTests.m
+++ Tests/KSWriterTests/KSBufferingTests.m
@@ -16,14 +16,39 @@
 
 
 @implementation KSBufferingTests
+{
+	KSWriter		*_writer;
+	NSMutableString *_output;
+}
+
+#pragma mark Harness
+
+- (void)performTestUsingBlock:(void (^)(KSWriter *writer))block;
+{
+	_output = [NSMutableString string];
+	_writer = [KSWriter writerWithMutableString:_output];
+	
+	block(_writer);
+	
+	// Clean up
+	_writer = nil;
+	_output = nil;
+}
+
+- (NSString *)string;	// returns what's been written so far
+{
+	return (_output ? _output : _writer.string);
+}
+
+#pragma mark Tests
 
 - (void)testNoBuffering;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer writeString:@"test"];
-	
-	STAssertEqualObjects(output, @"test", nil);
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer writeString:@"test"];
+		STAssertEqualObjects(self.string, @"test", nil);
+	}];
 }
 
 - (void)testAppend;
@@ -37,142 +62,150 @@
 
 - (void)testBeginBuffer;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	
-	STAssertTrue(writer.numberOfBuffers == 1, nil);
-	STAssertEqualObjects(output, @"", @"Buffer should stop anything being output");
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		
+		STAssertTrue(writer.numberOfBuffers == 1, nil);
+		STAssertEqualObjects(self.string, @"", @"Buffer should stop anything being self.string");
+	}];
 }
 
 - (void)testFlush;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	[writer writeString:@"ing"];	// make sure buffers are properly concatentated
-	[writer flush];
-	
-	STAssertTrue(writer.numberOfBuffers == 0, nil);
-	STAssertEqualObjects(output, @"testing", nil);
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		[writer writeString:@"ing"];	// make sure buffers are properly concatentated
+		[writer flush];
+		
+		STAssertTrue(writer.numberOfBuffers == 0, nil);
+		STAssertEqualObjects(self.string, @"testing", nil);
+	}];
 }
 
 - (void)testDiscardBuffer;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	[writer discardBuffer];
-	
-	STAssertTrue(writer.numberOfBuffers == 0, nil);
-	STAssertEqualObjects(output, @"", @"Buffer should stop anything being output");
-	
-	[writer writeString:@"test2"];
-	
-	STAssertEqualObjects(output, @"test2", @"test should be discarded, ready for test2 to be written untouched");
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		[writer discardBuffer];
+		
+		STAssertTrue(writer.numberOfBuffers == 0, nil);
+		STAssertEqualObjects(self.string, @"beforebuffer", @"Buffer should stop anything being output");
+		
+		[writer writeString:@"test2"];
+		
+		STAssertEqualObjects(self.string, @"test2", @"test should be discarded, ready for test2 to be written untouched");
+	}];
 }
 
 - (void)testBeginNestedBuffer;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer beginBuffer];
-	STAssertTrue(writer.numberOfBuffers == 2, nil);
-
-	[writer writeString:@"test"];
-	STAssertTrue(writer.numberOfBuffers == 2, nil);
-	STAssertEqualObjects(output, @"", @"Buffer should stop anything being output");
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer beginBuffer];
+		STAssertTrue(writer.numberOfBuffers == 2, nil);
+		
+		[writer writeString:@"test"];
+		STAssertTrue(writer.numberOfBuffers == 2, nil);
+		STAssertEqualObjects(self.string, @"", @"Buffer should stop anything being output");
+	}];
 }
 
 - (void)testFlushNestedBuffers;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	[writer beginBuffer];
-	[writer writeString:@"test2"];
-	[writer flush];
-	
-	STAssertTrue(writer.numberOfBuffers == 0, nil);
-	STAssertEqualObjects(output, @"testtest2", nil);
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		[writer beginBuffer];
+		[writer writeString:@"test2"];
+		[writer flush];
+		
+		STAssertTrue(writer.numberOfBuffers == 0, nil);
+		STAssertEqualObjects(self.string, @"testtest2", nil);
+	}];
 }
 
 - (void)testFlushFirstBuffer;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	[writer beginBuffer];
-	[writer writeString:@"test2"];
-	STAssertTrue(writer.numberOfBuffers == 2, nil);
-
-	[writer flushFirstBuffer];
-	STAssertTrue(writer.numberOfBuffers == 1, nil);
-	STAssertEqualObjects(output, @"test", @"The first buffer should flush, but not the second");
-	
-	// Make sure the second buffer hasn't been destroyed/damaged
-	[writer flush];
-	STAssertTrue(writer.numberOfBuffers == 0, nil);
-	STAssertEqualObjects(output, @"testtest2", @"The first buffer should flush, but not the second");
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		[writer beginBuffer];
+		[writer writeString:@"test2"];
+		STAssertTrue(writer.numberOfBuffers == 2, nil);
+		
+		[writer flushFirstBuffer];
+		STAssertTrue(writer.numberOfBuffers == 1, nil);
+		STAssertEqualObjects(self.string, @"test", @"The first buffer should flush, but not the second");
+		
+		// Make sure the second buffer hasn't been destroyed/damaged
+		[writer flush];
+		STAssertTrue(writer.numberOfBuffers == 0, nil);
+		STAssertEqualObjects(self.string, @"testtest2", @"The first buffer should flush, but not the second");
+	}];
 }
 
 - (void)testDiscardNestedBuffer;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	STAssertTrue(writer.numberOfBuffers == 1, nil);
-	
-	[writer beginBuffer];
-	[writer writeString:@"test2"];
-	STAssertTrue(writer.numberOfBuffers == 2, nil);
-	
-	[writer discardBuffer];
-	STAssertTrue(writer.numberOfBuffers == 1, nil);
-
-	[writer flush];
-	STAssertTrue(writer.numberOfBuffers == 0, nil);
-	STAssertEqualObjects(output, @"test", @"The first buffer should have made it through, with the second discarded");
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		STAssertTrue(writer.numberOfBuffers == 1, nil);
+		
+		[writer beginBuffer];
+		[writer writeString:@"test2"];
+		STAssertTrue(writer.numberOfBuffers == 2, nil);
+		
+		[writer discardBuffer];
+		STAssertTrue(writer.numberOfBuffers == 1, nil);
+		
+		[writer flush];
+		STAssertTrue(writer.numberOfBuffers == 0, nil);
+		STAssertEqualObjects(self.string, @"test", @"The first buffer should have made it through, with the second discarded");
+	}];
 }
 
 - (void)testFlushOnWrite;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	[writer flushOnNextWrite];
-	STAssertTrue(writer.numberOfBuffers == 1, nil);
-	
-	[writer writeString:@""];
-	STAssertTrue(writer.numberOfBuffers == 1, @"An empty string should not trigger flushing");
-	
-	[writer writeString:@"ing"];
-	STAssertTrue(writer.numberOfBuffers == 0, nil);
-	STAssertEqualObjects(output, @"testing", nil);
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		[writer flushOnNextWrite];
+		STAssertTrue(writer.numberOfBuffers == 1, nil);
+		
+		[writer writeString:@""];
+		STAssertTrue(writer.numberOfBuffers == 1, @"An empty string should not trigger flushing");
+		
+		[writer writeString:@"ing"];
+		STAssertTrue(writer.numberOfBuffers == 0, nil);
+		STAssertEqualObjects(self.string, @"testing", nil);
+	}];
 }
 
 - (void)testCancelFlushOnWrite;
 {
-	NSMutableString *output = [NSMutableString string];
-	KSWriter *writer = [KSWriter writerWithMutableString:output];
-	[writer beginBuffer];
-	[writer writeString:@"test"];
-	[writer flushOnNextWrite];
-	STAssertTrue(writer.numberOfBuffers == 1, nil);
-	
-	[writer cancelFlushOnNextWrite];
-	[writer writeString:@"ing"];
-	STAssertTrue(writer.numberOfBuffers == 1, nil);
-	STAssertEqualObjects(output, @"", nil);
+	[self performTestUsingBlock:^(KSWriter *writer) {
+		
+		[writer beginBuffer];
+		[writer writeString:@"test"];
+		[writer flushOnNextWrite];
+		STAssertTrue(writer.numberOfBuffers == 1, nil);
+		
+		[writer cancelFlushOnNextWrite];
+		[writer writeString:@"ing"];
+		STAssertTrue(writer.numberOfBuffers == 1, nil);
+		STAssertEqualObjects(self.string, @"", nil);
+	}];
 }
 
 @end
