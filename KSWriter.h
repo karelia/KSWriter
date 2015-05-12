d21919bab40a808d0cff582ec03f9fe469c171ab
Mike Abdullah <me@mikeabdullah.net>
2015-05-12 16:35:28 +0200

    Modernise lots of documentation

--- KSWriter.h
+++ KSWriter.h
@@ -37,54 +37,112 @@ FOUNDATION_EXPORT const unsigned char KSWriterVersionString[];
 
 #pragma mark Building up Strings
 
-// Strings are buffered in-memory, to be retrieved by -string
-// Encoding provides a hint for efficiency of buffering
+/**
+ Strings are buffered in-memory, to be retrieved from .string
+ 
+ @param encoding The format of the internal buffer, so you might be able to pick a format that suits
+ your input strings particularly well.
+ */
 + (KSWriter *)stringWriterWithEncoding:(NSStringEncoding)encoding;
 
-// This can be one of the least efficient types of writer since it might have to create lots of temporary substrings for appending to the mutable string
+/**
+ Convenience for appending to an existing mutable string.
+ 
+ This can be one of the least efficient types of writer, since it might have to create lots of
+ temporary substrings for appending to the mutable string. A lot of the time, you'll probably find
+ +stringWriterWithEncoding: is simply more convenient.
+ */
 + (KSWriter *)writerWithMutableString:(NSMutableString *)string encoding:(NSStringEncoding)encoding __attribute((nonnull(1)));
 
 
 #pragma mark Encoding as Data
 
-// if precompose == YES, then Unicode Normalization Form C is applied to the output. This is handy for distributing to platforms which don't have as good unicode support as Apple's. More details at http://developer.apple.com/library/mac/#qa/qa1235/_index.html#//apple_ref/doc/uid/DTS10001757
-
+/**
+ Encodes the strings and appends them to `data` buffer.
+ 
+ @param encoding The encoding to use for the data buffer.
+ 
+ @param precompose If YES, then Unicode Normalization Form C is applied to the output. This is handy
+ for distributing to platforms which don't have as good unicode support as Apple's. More details at
+ http://developer.apple.com/library/mac/#qa/qa1235/_index.html#//apple_ref/doc/uid/DTS10001757
+ */
 + (KSWriter *)writerWithMutableData:(NSMutableData *)data encoding:(NSStringEncoding)encoding precomposeStrings:(BOOL)precompose;
 
+/**
+ Encodes the strings and writes to `outputStream`.
+ 
+ @param encoding The encoding to use.
+ 
+ @param precompose If YES, then Unicode Normalization Form C is applied to the output. This is handy
+ for distributing to platforms which don't have as good unicode support as Apple's. More details at
+ http://developer.apple.com/library/mac/#qa/qa1235/_index.html#//apple_ref/doc/uid/DTS10001757
+ */
 + (KSWriter *)writerWithOutputStream:(NSOutputStream *)outputStream
                             encoding:(NSStringEncoding)encoding
                    precomposeStrings:(BOOL)precompose;
 
 
 #pragma mark Forwarding Onto Another Writer
-// Encoding is taken from the target writer
+
+/**
+ Creates a write whose job is purely to forward on input strings to another writer.
+ 
+ The new writer's `.encoding` matches that of the target writer.
+ */
 + (KSWriter *)writerWithOutputWriter:(KSWriter *)output __attribute((nonnull(1)));
 
 
 #pragma mark Custom Writing
 
-// The block is called for each string to be written
-// If the block isn't explicitly encoding data and you've nothing better to choose, go for NSUTF16StringEncoding
+/**
+ Create your own custom writer. For each input string to be written, your block is executed.
+ 
+ @param encoding If your block isn't explicitly encoding data and you've nothing better to choose,
+ go for NSUTF16StringEncoding
+ */
 + (KSWriter *)writerWithEncoding:(NSStringEncoding)encoding block:(void (^)(NSString *string, NSRange range))block __attribute((nonnull(2)));
 
-// Generally only useful for custom subclasses
-// Encoding may be used as a hint for buffering
+/**
+ Designated initializer for custom subclasses to use.
+ */
 - (instancetype)initWithEncoding:(NSStringEncoding)encoding NS_DESIGNATED_INITIALIZER;
 
 
 #pragma mark Writing
-- (void)writeString:(NSString *)string range:(NSRange)range;	// primitive
-- (void)writeString:(NSString *)string;							// convenience
+
+/**
+ The primitive writing API. If you need to write only a subrange of a string, calling here is often
+ much more efficient than creating substrings yourself.
+ 
+ @param string The source string to be written.
+ @param range The portion of `string` to be written.
+ */
+- (void)writeString:(NSString *)string range:(NSRange)range;
+
+/**
+ Of course, having to construct the correct range to use above gets tedious pretty quickly. Use this
+ API for convenience when writing whole strings.
+ */
+- (void)writeString:(NSString *)string;
 
 
 #pragma mark Properties
-@property(nonatomic, readonly) NSStringEncoding encoding;	// defaults to UTF-16 for non-data-based writers
+
+/**
+ The encoding the receiver uses. Depending on what the writer was created for, this may be the
+ output, or merely provide a hint for internal buffering.
+ 
+ Defaults to UTF-16 for non-data-based writers.
+ */
+@property(nonatomic, readonly) NSStringEncoding encoding;
 
 
 #pragma mark Output
 
-// A string comprised of all the non-buffered strings written so far
-// nil if the receiver wasn't created using `+stringWriterWithEncoding:`
+/**
+ For writers craeated using `+stringWriterWithEncoding:`, all the non-buffered output written so far.
+ `nil` for other writer types.
+ */
 @property (nonatomic, readonly, copy) NSString *string;
 
 
