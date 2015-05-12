543d1d06338a9ab21d3b9592e7920e5563856e87
Mike Abdullah <me@mikeabdullah.net>
2015-05-12 15:42:28 +0200

    Fresh new KSWriter framework

---
+++ KSWriterTests/KSWriterTests.m
@@ -0,0 +1,40 @@
+//
+//  KSWriterTests.m
+//  KSWriterTests
+//
+//  Created by Mike on 12/05/2015.
+//  Copyright (c) 2015 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface KSWriterTests : XCTestCase
+
+@end
+
+@implementation KSWriterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
